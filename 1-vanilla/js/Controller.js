6f5667a3bfb2a769741fad7d698bef6ceb5075bc
chaerimjo-web <chaerim010710@gmail.com>
2025-05-08 15:28:07 +0900

    검색어 입력 값 받기

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -3,8 +3,18 @@ const tag = "[Controller]";
 export default class Controller {
   constructor(store, { searchFormView }) {
     console.log(tag);
-    this.store = store;
 
+    this.store = store;
     this.searchFormView = searchFormView;
+
+    this.subscribeViewEvents();
+  }
+  subscribeViewEvents() {
+    this.searchFormView.on("@submit", (event) =>
+      this.search(event.detail.value)
+    );
+  }
+  search(keyword) {
+    console.log(tag, keyword);
   }
 }
--- 1-vanilla/js/views/SearchFormView.js
+++ 1-vanilla/js/views/SearchFormView.js
@@ -5,6 +5,7 @@ const tag = "[SearchFormView]";
 
 export default class SearchFormView extends View {
   constructor() {
+    console.log(tag);
     super(qs("#search-form-view"));
 
     this.inputElement = qs("[type=text]", this.element);
@@ -20,11 +21,19 @@ export default class SearchFormView extends View {
   bindEvent() {
     on(this.inputElement, "keyup", () => this.handleKeyup());
     //.bind(this) => 클래스 메서드는 this가 엉뚱하게 바인딩될 수 있기 때문에, 안전하게 현재 인스턴스를 바인딩한다. handleKeyup() 안에서의 this가 항상 SearchFormView 인스턴스를 가리키도록 보장 this=searchformview
+    on(this.element, "submit", (event) => this.handleSubmit(event));
   }
 
   handleKeyup() {
-    console.log(tag, "handleKeyup", this.inputElement.value);
-		const { value } = this.inputElement;
-		this.showResetButton(value.length > 0);
+    const { value } = this.inputElement;
+    this.showResetButton(value.length > 0);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(tag, "handleSubmit");
+
+    const { value } = this.inputElement;
+    this.emit("@submit", { value }); //"@submit" 커스텀 이벤트 생성
   }
 }
