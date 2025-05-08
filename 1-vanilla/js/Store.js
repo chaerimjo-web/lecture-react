e3179c536dc237602e309d077e6f7b01bad8d898
chaerimjo-web <chaerim010710@gmail.com>
2025-05-08 17:40:20 +0900

    검색결과 구현

--- 1-vanilla/js/Store.js
+++ 1-vanilla/js/Store.js
@@ -6,5 +6,14 @@ export default class Store {
     if (!storage) throw "no storage";
 
     this.storage = storage;
+
+    this.searchKeword = "";
+    this.searchResult = [];
+  }
+  search(keyword) {
+    this.searchKeword = keyword;
+    this.searchResult = this.storage.productData.filter((product) =>
+      product.name.includes(keyword)
+    );
   }
 }
