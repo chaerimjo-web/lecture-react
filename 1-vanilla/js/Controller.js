ad7820d9f12b1afe168058ee8b804f258c75fc41
chaerimjo-web <chaerim010710@gmail.com>
2025-05-09 14:57:00 +0900

    검색결과 값 출력

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,23 +1,33 @@
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView }) {
+  constructor(store, { searchFormView, searchResultView }) {
     console.log(tag);
 
     this.store = store;
     this.searchFormView = searchFormView;
+    this.searchResultView = searchResultView;
 
     this.subscribeViewEvents();
   }
   subscribeViewEvents() {
-    this.searchFormView.on("@submit", (event) =>
-      this.search(event.detail.value)
-    ).on("@reset", () => this.reset());
+    this.searchFormView
+      .on("@submit", (event) => this.search(event.detail.value))
+      .on("@reset", () => this.reset());
   }
-  search(keyword) {
-    console.log(tag, keyword);
+  search(searchKeword) {
+    // console.log(tag, keyword);
+    this.store.search(searchKeword);
+    this.render();
   }
   reset() {
     console.log(tag, "reset");
+  }
+  render() {
+    if (this.store.searchKeword.length > 0) {
+      this.searchResultView.show(this.store.searchResult);
+      return;
+    }
+    this.searchResultView.hide();
   }
 }
