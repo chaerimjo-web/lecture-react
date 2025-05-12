8b8289d6d06050877e761706a1e9d7681e02cf12
chaerimjo-web <chaerim010710@gmail.com>
2025-05-12 11:33:43 +0900

    TabView 렌더링

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -1,14 +1,18 @@
+import TabView from "./views/TabView.js";
+
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView }) {
+  constructor(store, { searchFormView, searchResultView, tabView }) {
     console.log(tag);
 
     this.store = store;
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
+    this.tabView = tabView;
 
     this.subscribeViewEvents();
+    this.render();
   }
   subscribeViewEvents() {
     this.searchFormView
@@ -28,9 +32,11 @@ export default class Controller {
   }
   render() {
     if (this.store.searchKeword.length > 0) {
+      this.tabView.hide();
       this.searchResultView.show(this.store.searchResult);
       return;
     }
+    this.tabView.show(); //처음 서치키워드 값이 없기 때문에 tabview가 보이고 searchResultView가 숨겨진다.
     this.searchResultView.hide();
   }
 }
