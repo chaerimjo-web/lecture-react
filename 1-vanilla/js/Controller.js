70184091c5c583f8676f40280869a28a6fd38c5c
chaerimjo-web <chaerim010710@gmail.com>
2025-05-12 16:58:49 +0900

    화면에 노출 및 숨기기

--- 1-vanilla/js/Controller.js
+++ 1-vanilla/js/Controller.js
@@ -3,18 +3,22 @@ import { TabType } from "./views/TabView.js";
 const tag = "[Controller]";
 
 export default class Controller {
-  constructor(store, { searchFormView, searchResultView, tabView }) {
+  constructor(
+    store,
+    { searchFormView, searchResultView, tabView, keywordListView }
+  ) {
     console.log(tag);
 
     this.store = store;
     this.searchFormView = searchFormView;
     this.searchResultView = searchResultView;
     this.tabView = tabView;
+    this.keywordListView = keywordListView;
 
     this.subscribeViewEvents();
     this.render();
   }
-  
+
   subscribeViewEvents() {
     this.searchFormView
       .on("@submit", (event) => this.search(event.detail.value))
@@ -45,12 +49,24 @@ export default class Controller {
     if (this.store.searchKeword.length > 0) {
       return this.renderSearchResult();
     }
+
     this.tabView.show(this.store.selectedTab);
+
+    if (this.store.selectedTab === TabType.KEYWORD) {
+      this.keywordListView.show(this.store.getKeywordList());
+    } else if (this.store.selectedTab === TabType.HISTORY) {
+      this.keywordListView.hide();
+    } else {
+      throw "사용할 수 없는 탭입니다.";
+    }
+
     this.searchResultView.hide();
   }
 
   renderSearchResult() {
     this.tabView.hide();
+    this.keywordListView.hide();
+
     this.searchResultView.show(this.store.searchResult);
   }
 }
