31f06ed46f2370d6aa9d81196d632838c1c8153a
chaerimjo-web <chaerim010710@gmail.com>
2025-05-15 11:36:58 +0900

    최근 검색어 추가

--- 1-vanilla/js/main.js
+++ 1-vanilla/js/main.js
@@ -1,6 +1,7 @@
 import Controller from "./Controller.js";
 import Store from "./Store.js";
 import storage from "./storage.js";
+import HistoryListView from "./views/HistoryListView.js";
 import KeywordListView from "./views/KeywordListView.js";
 import SearchFormView from "./views/SearchFormView.js";
 import SearchResultView from "./views/SearchResultView.js";
@@ -19,7 +20,8 @@ function main() {
     searchFormView: new SearchFormView(),
     searchResultView: new SearchResultView(),
     tabView: new TabView(),
-    keywordListView: new KeywordListView()
+    keywordListView: new KeywordListView(),
+    historyListView: new HistoryListView(),
   };
 
   new Controller(store, views);
