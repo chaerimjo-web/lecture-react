b69b4d16974fa8db943c811de02a5a19344aad71
chaerimjo-web <chaerim010710@gmail.com>
2025-05-28 16:51:39 +0900

    value 추가

--- 3-component/src/components/SearchForm.js
+++ 3-component/src/components/SearchForm.js
@@ -3,7 +3,7 @@ import React from "react";
 const SearchForm = ({ onSubmit, onChange, onReset, value }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
-    onSubmit();
+    onSubmit(value);
   };
 
   const handleChangeInput = (event) => {
