0ee77ffaa6e50436ef8b3cf6de3ac76c56eda28a
chaerimjo-web <chaerim010710@gmail.com>
2025-05-28 16:49:34 +0900

    파일복사

---
+++ 3-component/src/Store.js
@@ -0,0 +1,61 @@
+import storage from "./storage.js";
+import { createNextId } from "./helpers.js";
+
+const tag = "[store]";
+
+class Store {
+  constructor(storage) {
+    console.log(tag);
+    if (!storage) throw "no storage";
+
+    this.storage = storage;
+  }
+  search(keyword) {
+    this.addHistory(keyword);
+    return (this.searchResult = this.storage.productData.filter((product) =>
+      product.name.includes(keyword)
+    ));
+  }
+
+  addHistory(keyword = "") {
+    keyword = keyword.trim();
+    if (!keyword) {
+      return;
+    }
+
+    const hasHistory = this.storage.historyData.some(
+      (history) => history.keyword === keyword
+    );
+    if (hasHistory) {
+      this.removeHistory(keyword);
+    }
+
+    const id = createNextId(this.storage.historyData);
+    const date = new Date();
+
+    this.storage.historyData.push({ id, keyword, date });
+    this.storage.historyData = this.storage.historyData.sort(this._sortHistory);
+  }
+
+  getKeywordList() {
+    return this.storage.keywordData;
+  }
+
+  getHistoryList() {
+    return this.storage.historyData.sort(this._sortHistory);
+  }
+
+  _sortHistory(history1, history2) {
+    return history2.date - history1.date;
+  }
+
+  removeHistory(keyword) {
+    this.storage.historyData = this.storage.historyData.filter(
+      (history) => history.keyword !== keyword
+      //검색 이력 중 특정 keyword와 일치하는 항목을 제거하는 기능
+    );
+  }
+}
+
+const store = new Store(storage);
+export default store;
