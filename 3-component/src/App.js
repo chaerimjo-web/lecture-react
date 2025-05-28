e837b85a5f50c182d1aa3cbdb29ac050debd24b5
chaerimjo-web <chaerim010710@gmail.com>
2025-05-28 11:22:45 +0900

    Header 생성

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
 
 export default class App extends React.Component {
-	render() {
-		return (<>TODO: 컴포넌트</>)
-	}
+  render() {
+    return (
+      <>
+        <Header />
+      </>
+    );
+  }
 }
