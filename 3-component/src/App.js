558b95e26a804b307cac883ca97663c6f24208e2
chaerimjo-web <chaerim010710@gmail.com>
2025-05-28 15:38:16 +0900

    이벤트 추가

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,26 +1,40 @@
 import React from "react";
 import Header from "./components/Header.js";
-import SearchForm from "./components/SearchForm.js"
+import SearchForm from "./components/SearchForm.js";
 
 export default class App extends React.Component {
-	search(searchKeyword) {
-		console.log(searchKeyword);
-	}
+  constructor() {
+    super();
 
-	handleReset() {
-		console.log('reset');
-	}
+    this.state = { searchKeyword: "" };
+  }
+  search(searchKeyword) {
+    console.log(searchKeyword);
+  }
+
+  handleReset() {
+    console.log("reset");
+  }
+
+  handleChangeInput(searchKeyword) {
+    if (searchKeyword.length <= 0) {
+      this.handleReset();
+    }
+    this.setState({ searchKeyword });
+  }
 
   render() {
     return (
       <>
         <Header title="검색" />
         <div className="container">
-					<SearchForm 
-						onSubmit={(searchKeyword) => this.search(searchKeyword)} 
-						onReset={() => this.handleReset()}
-					/>
-				</div>
+          <SearchForm
+            value={this.state.searchKeyword}
+            onChange={(value) => this.handleChangeInput(value)}
+            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            onReset={() => this.handleReset()}
+          />
+        </div>
       </>
     );
   }
