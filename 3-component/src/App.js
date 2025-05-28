1d05735bc889a46693f122046ab840e02ae3f221
chaerimjo-web <chaerim010710@gmail.com>
2025-05-28 14:27:20 +0900

    onSubmit, onReset 함수

--- 3-component/src/App.js
+++ 3-component/src/App.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Header from "./components/Header";
+import Header from "./components/Header.js";
+import SearchForm from "./components/SearchForm.js"
 
 export default class App extends React.Component {
+	search(searchKeyword) {
+		console.log(searchKeyword);
+	}
+
+	handleReset() {
+		console.log('reset');
+	}
+
   render() {
     return (
       <>
-        <Header />
+        <Header title="검색" />
+        <div className="container">
+					<SearchForm 
+						onSubmit={(searchKeyword) => this.search(searchKeyword)} 
+						onReset={() => this.handleReset()}
+					/>
+				</div>
       </>
     );
   }
