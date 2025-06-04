d6a8717db99dcf2d02143abef0dab98a01b3aec9
chaerimjo-web <chaerim010710@gmail.com>
2025-06-04 15:41:36 +0900

    클래스형으로 변경

--- 3-component/src/components/KeywordList.js
+++ 3-component/src/components/KeywordList.js
@@ -2,20 +2,36 @@ import store from "../Store.js";
 import List from "./List.js";
 import React from "react";
 
-export default class KeywordList extends List {
+export default class KeywordList extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      keywordList: [],
+    };
+  }
+
   componentDidMount() {
-    const data = store.getKeywordList();
+    const keywordList = store.getKeywordList();
     this.setState({
-      data,
+      keywordList,
     });
   }
 
-  renderItem(item, index) {
-    return (
-      <>
-        <span className="number">{index + 1}</span>
-        <span className="keyword">{item.keyword}</span>
-      </>
-    );
+  render() {
+    return(
+      <List
+        data={this.state.keywordList}
+        onClick={this.props.onClick}
+        renderItem={(item, index) => {
+          return (
+            <>
+              <span className="number">{index + 1}</span>
+              <span className="keyword">{item.keyword}</span>
+            </>
+          );
+        }}
+      />
+    )
   }
 }
